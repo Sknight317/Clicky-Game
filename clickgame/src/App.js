94c698b8c4bfed890a1cb8cd3a806c1867985269
Sknight317 <Sknight23@mail.valenciacollege.edu>
2019-01-17 16:24:17 -0500

    css

--- clickgame/src/App.js
+++ clickgame/src/App.js
@@ -3,7 +3,7 @@ import ImageCard from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
 import image from "./image.json";
 import Header from "./components/Header"
-
+import Navbar from "./components/Navbar"
 
 class App extends Component {
   // Setting this.state.image to the image json array
@@ -21,33 +21,34 @@ clickImage = id => {
   //Sets constant to the shuffle array function to shuffle images
  const shuffle = this.shuffleArray(image);
  this.setState({image: shuffle});
+ const contains = this.state.clicked.includes(id)
  //If the id of the image that was just clicked has already been clicked
- if(this.state.clicked.includes(id)) {
-   //Empty the clicked array; tell the user they guessed incorrectly
+ if(!contains) {
+    this.setState({
+    //Merge the image that has just been clicked into the clicked array
+    clicked: this.state.clicked.concat([id]),
+    score: this.state.score + 1,
+    message: "Correct!",
+    move: "false" 
+});
+   
+ }
+ else {
+this.endGame()
+}//If the current score is greater than the top score; change the topscore to the number of the score
+if (this.state.score > this.state.topScore) {
+  this.setState({ topScore: this.state.score });
+ }
+}
+endGame= () => {
+//Empty the clicked array; tell the user they guessed incorrectly
    this.setState({score: 0,
                   clicked: [],
                   message: "You guessed incorrectly!",
                   move: "true"
 
   })
- 
- }
- else {
-  this.setState({
-    //Merge the image that has just been clicked into the clicked array
-    clicked: this.state.clicked.concat([id]),
-    score: this.state.score + 1,
-    message: "Correct!",
-    move: "false"
-    
-});
-
-}//If the current score is greater than the top score; change the topscore to the number of the score
-if (this.state.score > this.state.topScore) {
-  this.setState({ topScore: this.state.score });
- }
 }
-
  //Function to rearrange the images
 shuffleArray = (image) => {
   for (let i = image.length - 1; i > 0; i--) {
@@ -62,13 +63,16 @@ shuffleArray = (image) => {
   render() {
     return (
       
+      
       <Wrapper>
-        
+       <Navbar
+        score={this.state.score}
+        message={this.state.message}
+        topScore={this.state.topScore}
+        />   
          
         <Header>
-        <p>Score: {this.state.score}</p>
-        <p> {this.state.message} </p>
-        <p>Top Score: {this.state.topScore} </p>
+      
         </Header>
         {this.state.image.map(image => (
           <ImageCard
--- clickgame/src/components/Navbar/index.js
+++ clickgame/src/components/Navbar/index.js
@@ -1,26 +1,17 @@
 import React from "react";
 import "./style.css";
 
-function Navbar(props) {
-  return <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-        <li class="nav-item active">
-            <p>Click an Image to Begin<span class="score"></span></p>
-          </li>
-      <li class="nav-item active">
-        <p>Score:<span class="score">{props.score}</span></p>
-      </li>
-      <li class="nav-item active">
-        <p>Top Score:<span class="top-score">{props.topscore}</span></p>
-      </li>
-    </ul>
-  </div>
-</nav> 
-}
-
+const Navbar = props => (
+    <nav className="navbar-style">
+      <ul>
+        <p>Animal Clicky Game</p>
+  
+        <li id="message">{props.message}</li>
+  
+        <li id="score">Score: {props.score}</li> |
+  
+        <li id="top-score">Top Score: {props.topScore}</li>
+      </ul>
+    </nav>
+  );
 export default Navbar;
